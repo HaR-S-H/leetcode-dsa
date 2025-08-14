1eb95aa265b71778710854e50449c6207a11d9b7
HaR-S-H <119717186+HaR-S-H@users.noreply.github.com>
2025-08-14 18:58:10 +0530

    Time: 1 ms (72.42%), Space: 46 MB (15.84%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,25 +1,46 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
         int m=matrix.length;
-       int row[]=new int[m];
-       int n=matrix[0].length;
-       int col[]=new int[n];
-       Arrays.fill(row,1);
-       Arrays.fill(col,1);
-       for(int i=0;i<m;i++){
-           for(int j=0;j<n;j++){
-               if(matrix[i][j]==0){
-                   row[i]=0;
-                   col[j]=0;
-               }
-           }
-       }
-       for(int i=0;i<m;i++){
-           for(int j=0;j<n;j++){
-               if(row[i]==0 || col[j]==0){
-                   matrix[i][j]=0;
-               }
-           }
-       }
+        int n=matrix[0].length;
+        boolean firstRow=false;
+        boolean firstCol=false;
+        for(int col=0;col<n;col++){
+            if(matrix[0][col]==0){
+                firstRow=true;
+                break;
+            }
+        }
+        for(int row=0;row<m;row++){
+            if(matrix[row][0]==0){
+                firstCol=true;
+                break;
+            }
+        }
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(matrix[i][j]==0){
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
+                }
+            }
+        }
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(matrix[i][0]==0 || matrix[0][j]==0){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+        if(firstRow){
+            for(int col=0;col<n;col++){
+                matrix[0][col]=0;
+            }
+        }
+        if(firstCol){
+            for(int row=0;row<m;row++){
+                matrix[row][0]=0;
+            }
+        }
+        // return matrix;
     }
 }
